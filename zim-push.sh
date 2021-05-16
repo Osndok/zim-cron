1b36729b262f13774224658e33d181fad1edb880
Robert Hailey <git@osndok.com>
2021-05-16 08:58:38 -0500

    Push logic is now in separate file

--- zim-push.sh
+++ zim-push.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 #
-# zim-commit.sh - run regularly from a cron script to loosely maintain local & remote zim git repos.
-# Will KILL zim if remote changes are detected to avoid the 'cannot save page' errors.
-# Will try to produce meaningful git commit messages.
+# zim-push.sh - pushes all git-backed notebook repos to all remote branches
 #
-# To work at all, the remotes must not require the manual entry of a password, and the local 'master'
-# branch should be renamed "$(hostname)".
-#
-# When combined with the pre-launch script (that crudely auto-merges remote tracking branches into
-# the current one) this is intended to possibly provide easy one-person-multiple-machine synchronized
-# zim repos (where you just "launch zim and it works").
-#
-
-# Longest common prefix (optional, but makes for even nicer commit messages)
-LCP=$HOME/bin/lcp
-test -e "$LCP" || LCP=$HOME/.local/bin/lcp
-test -e "$LCP" || LCP=/usr/local/bin/lcp
-test -e "$LCP" || LCP=/usr/bin/lcp
 
 NOTEBOOKS=$HOME/Notebooks
 
@@ -47,7 +32,7 @@ cd $NOTEBOOK
 
 test -d .git || git init
 
-LOCK=.git/autocommit.pid
+LOCK=.git/autopush.pid
 
 if [ -e "$LOCK" ]
 then
@@ -161,86 +146,46 @@ function any_other_branch_is_newer_or_divergent()
 	return 1
 }
 
-function maybe_commit_changes()
+function send_changes()
 {
-	if ! git add -A .
+	git remote | while read UPSTREAM
+	do
+		git push --all "$UPSTREAM" || echo "Upstream push failed"
+		git push -f "$UPSTREAM" "$HOSTNAME":"$HOSTNAME" || echo "Unable to push authoritative changes to $UPSTREAM"
+	done
+}
+
+function send_local_changes()
+{
+	local HERE=$(git rev-parse "$HOSTNAME")
+	local THERE;
+
+	if [ "$HERE" == "$HOSTNAME" ]
 	then
-		# git add is a simple operation, and should always work. The only time I have had it
-		# fail is with a corrupt repo. We don't want to silently let work continue on this repo,
-		# and it is a bit more hairy to parse diagnostic output for expected failure cases. So
-		# instead, we will take this failure as a sign that things are BAD... REALLY BAD.
-		# Maybe a full disk? Maybe a zeroed git object? It's hard to tell.
-		echo 1>&2 "git-add does not work, so cowardly refusing to continue."
-		# Get the operator's attention... eventually. There work is not being persisted.
-		panic "etc/cron.hourly/zim-commit.sh: git-add failed, killing zim b/c work may no longer being saved"
-		# Hopefully they won't just keep launching zim every hour/30mins.
-		kill_zim
-		exit 3
+		echo 1>&2 "recovering from possibly-incorrect local branch checkout"
+		git checkout -b "$HOSTNAME" || git checkout "$HOSTNAME"
+		HERE=$(git rev-parse "$HOSTNAME")
 	fi
 
-	DIRTY_FILES="$(git status --porcelain)"
-
-	if [ -n "$DIRTY_FILES" ]
-	then
-		MESSAGE=""
-
-		if [ -x "$LCP" ]
+	git remote | while read UPSTREAM
+	do
+		THERE=$(git rev-parse "remotes/$UPSTREAM/$HOSTNAME")
+		if [ "$HERE" != "$THERE" ]
 		then
-			# Journal entries are second-class citizens here, so first... let's try to get a prefix *ignoring* all the Journal entries
-			PREFIX=$(git status --porcelain | cut -c4- | grep -v '^Journal' | $LCP)
-			# If the lcp is an actual file or directory, then we use it, otherwise ignore it.
-			# This will let us have more descriptive commit messages when all the changes are
-			# related, without introducing non-sensical word-bit prefixes from unrelated paths.
-			if [ -e "$PREFIX" ]
-			then
-				MESSAGE=":$(echo $PREFIX | sed -e 's/\.txt$//g' | tr '/' ':')"
-			else
-				echo "Unusable PREFIX 1: $PREFIX"
-			fi
-
-			# If filtering out the Journal entries leaves us with nothing, then maybe we can try again and still get
-			# more detailed journal path than the old way, which would just present us with "Journal" as a commit message.
-			if [ -z "$MESSAGE" ]
-			then
-				PREFIX=$(git status --porcelain | cut -c4- | $LCP)
-				# If the lcp is an actual file or directory, then we use it, otherwise ignore it.
-				# This will let us have more descriptive commit messages when all the changes are
-				# related, without introducing non-sensical word-bit prefixes from unrelated paths.
-				if [ -e "$PREFIX" ]
-				then
-					MESSAGE=":$(echo $PREFIX | sed -e 's/\.txt//g' | tr '/' ':')"
-				else
-					echo "Unusable PREFIX 2: $PREFIX"
-				fi
-			fi
-
-			# Since the below-action will trim the last character of the string, we must add a character... unless we already have an extra one.
-			if [ -n "$MESSAGE" ] && [[ "$MESSAGE" != *: ]]
-			then
-				MESSAGE="$MESSAGE,"
-			fi
+			git push -f $UPSTREAM $HOSTNAME:$HOSTNAME || echo "Unable to push authoritative local changes to $UPSTREAM"
 		fi
+	done
+}
 
-		if [ -z "$MESSAGE" ]
-		then
-		#MESSAGE=$(git status --porcelain | sed -e 's/\.txt//g' | tr / '\t' | cut -c4- | uniq -w3 | cut -f1 | tr -s '[:space:]' , )
-		MESSAGE=$(git status --porcelain | tr '/. ' '\t' | cut -c4- | uniq -w3 | cut -f1 | tr -s '[:space:]' , )
-		fi
-
-		if [ -z "$MESSAGE" ]
-		then
-			MESSAGE='cron.hourly: zim-commit.shX'
-		fi
-
-		# 'Journal' (by itself) is a bit uninteresting... unless it is the only thing changed!
-		if [ "$MESSAGE" != "Journal," ]
-		then
-			# Limit the message length to 512 characters
-			MESSAGE=$(echo "$MESSAGE" | cut -c-512 | sed -E 's/,Journal|Journal,//g')
-		fi
-
-		git commit -m"${MESSAGE%?}"
-	fi
+function maybe_send_changes()
+{
+	#todo: if we seem to be the leader, then push to main
+	#then
+	#	time send_changes
+	#else
+		# MAYBE... we created one or more commits, but the connection was not available (at the time)?
+		send_local_changes
+	#fi
 }
 
 function fetched_something_new()
@@ -291,6 +236,13 @@ function fast_forward_master_branch()
 	fi
 }
 
+# There are parallel branch names EVERYWHERE... so if ever we update our local branch, it can
+# help to speed things along by sending an 'ack'... which just shifts the remote side's perception
+# of where this side's branch tracking is... in practice, this is just another PUSH, but with no data.
+function send_ack()
+{
+	maybe_send_changes
+}
 
 # ------------------------------------------------------------------------------
 
@@ -304,22 +256,25 @@ then
 
 	# If we are "running *THE* process", then we do *NOT* delay our execution.
 	# That way, we will form a commit sooner, so that the other scripts can "pass it around"
-	maybe_commit_changes
+	maybe_send_changes
 
 	if any_other_branch_is_newer_or_divergent
 	then
 		echo "At least one incoming branch was updated, killing zim..."
 		kill_zim
 		fast_forward_where_possible
+		send_ack
 	elif fetched_something_new
 	then
 		echo "Noticed & pulling down remote changes, killing zim..."
 		kill_zim
 		fast_forward_where_possible
 		fast_forward_master_branch
+		send_ack
 	else
 		echo "Nothing broken to report or take action on; let zim be..."
 		fast_forward_master_branch
+		send_ack
 	fi
 else
 	echo "zim is *NOT* running"
@@ -334,12 +289,13 @@ else
 	# TODO: we could also choose not to delay our execution if we find our local repo has changes
 	# Why so high? It is critical to out-wait the originator, or else we will be up to two full period behind (two hours)
 	tty || sleep 33
-	maybe_commit_changes
+	maybe_send_changes
 
 	if fetched_something_new
 	then
 		echo "Remote changes detected"
 		fast_forward_where_possible
+		send_ack
 	fi
 fi
 
